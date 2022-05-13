97d68fcef5d31b43db4089d95e705f78178219f5
root <dnwls2343@gmail.com>
2022-05-13 13:58:03 +0000

    first

---
+++ fedml_api/data_preprocessing/coco/data_loader.py
@@ -0,0 +1,132 @@
+import logging
+
+import numpy as np
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+
+from .datasets import CocoDataset
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def read_data_distribution(filename='./data_preprocessing/non-iid-distribution/CIFAR10/distribution.txt'):
+    distribution = {}
+    with open(filename, 'r') as data:
+        for x in data.readlines():
+            if '{' != x[0] and '}' != x[0]:
+                tmp = x.split(':')
+                if '{' == tmp[1].strip():
+                    first_level_key = int(tmp[0])
+                    distribution[first_level_key] = {}
+                else:
+                    second_level_key = int(tmp[0])
+                    distribution[first_level_key][second_level_key] = int(tmp[1].strip().replace(',', ''))
+    return distribution
+
+def read_net_dataidx_map(filename='./data_preprocessing/non-iid-distribution/CIFAR10/net_dataidx_map.txt'):
+    net_dataidx_map = {}
+    with open(filename, 'r') as data:
+        for x in data.readlines():
+            if '{' != x[0] and '}' != x[0] and ']' != x[0]:
+                tmp = x.split(':')
+                if '[' == tmp[-1].strip():
+                    key = int(tmp[0])
+                    net_dataidx_map[key] = []
+                else:
+                    tmp_array = x.split(',')
+                    net_dataidx_map[key] = [int(i.strip()) for i in tmp_array]
+    return net_dataidx_map
+
+def record_net_data_stats(y_train, net_dataidx_map):
+    net_cls_counts = {}
+
+    for net_i, dataidx in net_dataidx_map.items():
+        unq, unq_cnt = np.unique(y_train[dataidx], return_counts=True)
+        tmp = {unq[i]: unq_cnt[i] for i in range(len(unq))}
+        net_cls_counts[net_i] = tmp
+    logging.debug('Data statistics: %s' % str(net_cls_counts))
+    return net_cls_counts
+
+def load_coco_data(datadir):
+    train_transform, test_transform = _data_transforms_coco()
+
+    coco_train_ds = CocoDataset(datadir, train=True, download=True, transform=train_transform)
+    coco_test_ds = CocoDataset(datadir, train=False, download=True, transform=test_transform)
+
+    X_train, y_train = coco_train_ds.data, coco_train_ds.target
+    X_test, y_test = coco_test_ds.data, coco_test_ds.target
+
+    return (X_train, y_train, X_test, y_test)
+
+def partition_data(dataset, datadir, partition, n_nets, alpha):
+    logging.info("*********partition data***************")
+    X_train, y_train, X_test, y_test = load_coco_data(datadir)
+    n_train = X_train.shape[0]
+    # n_test = X_test.shape[0]
+
+    if partition == "homo":
+        total_num = n_train
+        idxs = np.random.permutation(total_num)
+        batch_idxs = np.array_split(idxs, n_nets)
+        net_dataidx_map = {i: batch_idxs[i] for i in range(n_nets)}
+
+    elif partition == "hetero":
+        min_size = 0
+        K = 10
+        N = y_train.shape[0]
+        logging.info("N = " + str(N))
+        net_dataidx_map = {}
+
+        while min_size < 10:
+            idx_batch = [[] for _ in range(n_nets)]
+            # for each class in the dataset
+            for k in range(K):
+                idx_k = np.where(y_train == k)[0]
+                np.random.shuffle(idx_k)
+                proportions = np.random.dirichlet(np.repeat(alpha, n_nets))
+                ## Balance
+                proportions = np.array([p * (len(idx_j) < N / n_nets) for p, idx_j in zip(proportions, idx_batch)])
+                proportions = proportions / proportions.sum()
+                proportions = (np.cumsum(proportions) * len(idx_k)).astype(int)[:-1]
+                idx_batch = [idx_j + idx.tolist() for idx_j, idx in zip(idx_batch, np.split(idx_k, proportions))]
+                min_size = min([len(idx_j) for idx_j in idx_batch])
+
+        for j in range(n_nets):
+            np.random.shuffle(idx_batch[j])
+            net_dataidx_map[j] = idx_batch[j]
+
+    elif partition == "hetero-fix":
+        dataidx_map_file_path = './data_preprocessing/non-iid-distribution/CIFAR10/net_dataidx_map.txt'
+        net_dataidx_map = read_net_dataidx_map(dataidx_map_file_path)
+
+    if partition == "hetero-fix":
+        distribution_file_path = './data_preprocessing/non-iid-distribution/CIFAR10/distribution.txt'
+        traindata_cls_counts = read_data_distribution(distribution_file_path)
+    else:
+        traindata_cls_counts = record_net_data_stats(y_train, net_dataidx_map)
+
+    return X_train, y_train, X_test, y_test, net_dataidx_map, traindata_cls_counts
+
+# for centralized training
+def get_dataloader(dataset, datadir, train_bs, test_bs, dataidxs=None):
+    return get_dataloader_coco(datadir, train_bs, test_bs, dataidxs)
+
+
+# for local devices
+def get_dataloader_test(dataset, datadir, train_bs, test_bs, dataidxs_train, dataidxs_test):
+    return get_dataloader_test_coco(datadir, train_bs, test_bs, dataidxs_train, dataidxs_test)
+
+def get_dataloader_coco(datadir, train_bs, test_bs, dataidxs=None):
+    dl_obj = CocoDataset
+
+    transform_train, transform_test = _data_transforms_cifar10()
+
+    train_ds = dl_obj(datadir, dataidxs=dataidxs, train=True, transform=transform_train, download=True)
+    test_ds = dl_obj(datadir, train=False, transform=transform_test, download=True)
+
+    train_dl = data.DataLoader(dataset=train_ds, batch_size=train_bs, shuffle=True, drop_last=True)
+    test_dl = data.DataLoader(dataset=test_ds, batch_size=test_bs, shuffle=False, drop_last=True)
+
+    return train_dl, test_dl
